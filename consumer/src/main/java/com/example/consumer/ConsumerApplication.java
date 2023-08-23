98ec6ba2e579f858644db8a05921cdbaa7fa8ba9
Pedro Luis Pereira Atencio <pedro.pereira@ceiba.com.co>
2023-08-23 12:01:29 -0500

    ok

--- consumer/src/main/java/com/example/consumer/ConsumerApplication.java
+++ consumer/src/main/java/com/example/consumer/ConsumerApplication.java
@@ -3,11 +3,13 @@ package com.example.consumer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
 @EnableCircuitBreaker
 @SpringBootApplication
+@EnableHystrixDashboard
 public class ConsumerApplication {
 
 	public static void main(String[] args) {
